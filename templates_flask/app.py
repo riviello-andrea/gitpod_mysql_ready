670476085ec5a2a19d5ba3534f5c12f312ed99d7
riviello-andrea <riviello.andrea@iisgalvanimi.edu.it>
2023-03-24 10:46:42 +0000

    v3

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,10 +1,21 @@
 from flask import render_template
 from flask import Flask
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="CLASH_ROYALE"
+)
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return render_template('hello.html', name='Andrea')
+def unitList():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult=mycursor.fetchall()
+    return render_template('clash_units.html', units=myresult)
